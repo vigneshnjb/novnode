0fdb3e71ff70e4b1bd6bfe8603712b5a62d927ab
vigneshnjb <vigneshnjb666@gmail.com>
2021-11-15 12:34:02 +0530

    afd

--- app.js
+++ app.js
@@ -118,6 +118,7 @@ app.get('/menu/:id',(req,res) =>{
 })
 
 app.post('/menuitem',(req,res) => {
+    console.log(req.body)
     db.collection('menu').find({menu_id:{$in:req.body.ids}}).toArray((err,result)=>{
         if(err) throw err;
         res.send(result)
