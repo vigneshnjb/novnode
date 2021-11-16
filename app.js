798a114864cd623304671b98a4d733e90a7f4153
vigneshnjb <vigneshnjb666@gmail.com>
2021-11-16 13:05:20 +0530

    cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const app = express();
+const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config()
 const bodyParser = require('body-parser');
